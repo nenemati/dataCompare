e28a5a9ce4b9cbb1a5305d69a6741b008927582c
Nema Nemati <nenemati@gmail.com>
2015-06-11 22:24:29 -0400

    database helper class

---
+++ database.py
@@ -0,0 +1,45 @@
+__author__ = 'nemanemati'
+
+import MySQLdb
+
+class myDb(object):
+
+    def __init__(self, hostname, username, password, database):
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.database = database
+
+    def getHostname(self):
+        return self.hostname
+
+    def getUsername(self):
+        return self.username
+
+    def getPassword(self):
+        return self.password
+
+    def getDatabase(self):
+        return self.database
+
+    def connect(self):
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
