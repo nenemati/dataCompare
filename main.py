5f25f174228f1696e743531b7b1353d62e91fdb0
Nema Nemati <nenemati@gmail.com>
2015-06-14 18:13:05 -0400

    deleted data set

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import csv
+import pandas as pd
+from pandas import DataFrame
 
 
-f = open('LoanStats3d.csv', 'rb')
-reader = csv.reader(f)
 
-for row in reader:
-    print(row)
-f.close()
 
+#
+# f = open('LoanStats3d.csv', 'rb')
+# reader = csv.reader(f)
+#
+# for row in reader:
+#     print(row)
+# f.close()
+#
 
