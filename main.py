6ac64228ae2b05b72076e3159447658d03f2035c
Nema Nemati <nenemati@gmail.com>
2015-06-14 19:44:27 -0400

    added new data set need to change name got some modules going

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import pandas as pd
 from pandas import DataFrame
 
 
+df1 = pd.read_csv('Relocated_Vehicles.csv')
+df1 = pd.read_csv('Towed_Vehicles.csv')
+
+print df1.head(2)
+
+
+
+
+
+
 
 
 #
---
+++ regression.py
@@ -0,0 +1,6 @@
+__author__ = 'nemanemati'
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
