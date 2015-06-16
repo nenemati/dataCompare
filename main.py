aee1b452ed23b2d22801057d83c1717058f0570e
Nema Nemati <nenemati@gmail.com>
2015-06-15 23:00:48 -0400

    some functions

--- main.py
+++ main.py
@@ -3,10 +3,28 @@ import pandas as pd
 from pandas import DataFrame
 
 
-df1 = pd.read_csv('Relocated_Vehicles.csv')
-df1 = pd.read_csv('Towed_Vehicles.csv')
+# df1 = pd.read_csv('Relocated_Vehicles.csv')
+# df1 = pd.read_csv('Towed_Vehicles.csv')
+#
+def square(list):
+    newList = []
+    for i in list:
+        newList.append(i ** 2)
+    return newList
 
-print df1.head(2)
+def multiplyList(list1,list2):
+    newlist = [a*b for a,b in zip(list1,list2)]
+    return newlist
+
+
+
+
+list1 = [-1,1,2,4,6,7]
+list2 = [-1,2,3,3,5,8]
+listXSquared = square(list1)
+listYSqared = square(list2)
+listXtimeY = multiplyList(list1,list2)
+
 
 
 
