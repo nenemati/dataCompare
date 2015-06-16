5cd7da85531a7b68dc241e1e3e565898ecf978e3
Nema Nemati <nenemati@gmail.com>
2015-06-16 00:58:27 -0400

    so values are right

--- main.py
+++ main.py
@@ -28,11 +28,17 @@ def findSlope(listX, listY):
     import math
     sigmaX = addElementsInList(listX)
     sigmaY = addElementsInList(listY)
+    sigmaXY = multiplyList(listX,listY)
     n = len(list1) # might want to make a f(x) call here
     sigmaXthenSquared = math.pow(sum(listX),2)
     sigmaXSquared = sum(square(listX))
-    # slope = ((sigmaX*sigmaY) - (n*sigmaXY))/((sigmaXSquared) - (n*sigmaXSquared))
-    # return slope
+    numeratorFirst = sigmaX*sigmaY
+    numeratorSecond = n*sigmaXY
+    numerator = numeratorFirst - numeratorSecond
+    print(numerator)
+
+
+
 
 
 
