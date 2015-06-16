7e5c23bb71727c2a92d8d04b89e932028c81bddc
Nema Nemati <nenemati@gmail.com>
2015-06-16 00:47:29 -0400

    so values are right

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def addElementsInList(list):
     newlist = []
     for row in list:
         newlist = sum(list)
+    return newlist
 
 
 def findSlope(listX, listY):
@@ -28,27 +29,29 @@ def findSlope(listX, listY):
     sigmaX = addElementsInList(listX)
     sigmaY = addElementsInList(listY)
     n = len(list1) # might want to make a f(x) call here
-    sigmaXY = addElementsInList(multiplyList(listX,listY))
-    sigmaXSquared = math.pow(sigmaX,2)
-    
+    sigmaXthenSquared = math.pow(sum(listX),2)
+    sigmaXSquared = sum(square(listX))
+    # slope = ((sigmaX*sigmaY) - (n*sigmaXY))/((sigmaXSquared) - (n*sigmaXSquared))
+    # return slope
 
 
-    value = ((sumofListX*sumofListY) - (n*sumofListXtimeY))/((math.pow(sumofListX,2)) - (n*math.pow(sumofListX,2)))
-    return value
 
 
 list1 = [-1,1,2,4,6,7]
 list2 = [-1,2,3,3,5,8]
-listXSquared = square(list1)
-listYSquared = square(list2)
-listXtimeY = multiplyList(list1,list2)
-sumofListX = addElementsInList(list1)
-sumofListY = addElementsInList(list2)
-sumofListXSquared = addElementsInList(listXSquared)
-sumofListYSquared = addElementsInList(listYSquared)
-sumofListXtimeY = addElementsInList(listXtimeY)
-n = len(list1)
-print slope()
+
+print findSlope(list1,list2)
+
+# listXSquared = square(list1)
+# listYSquared = square(list2)
+# listXtimeY = multiplyList(list1,list2)
+# sumofListX = addElementsInList(list1)
+# sumofListY = addElementsInList(list2)
+# sumofListXSquared = addElementsInList(listXSquared)
+# sumofListYSquared = addElementsInList(listYSquared)
+# sumofListXtimeY = addElementsInList(listXtimeY)
+# n = len(list1)
+# print slope()
 
 
 
