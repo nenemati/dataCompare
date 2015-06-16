450b8e08ab4eac62ee93a44e7efea69f19c3b0e4
Nema Nemati <nenemati@gmail.com>
2015-06-15 23:34:07 -0400

    some more functions

--- main.py
+++ main.py
@@ -17,15 +17,22 @@ def multiplyList(list1,list2):
     return newlist
 
 
-
+def addElementsInList(list):
+    newlist = []
+    for row in list:
+        newlist = sum(list)
 
 list1 = [-1,1,2,4,6,7]
 list2 = [-1,2,3,3,5,8]
 listXSquared = square(list1)
-listYSqared = square(list2)
+listYSquared = square(list2)
 listXtimeY = multiplyList(list1,list2)
-
-
+sumofListX = addElementsInList(list1)
+sumofListY = addElementsInList(list2)
+sumofListXSquared = addElementsInList(listXSquared)
+sumofListYSquared = addElementsInList(listYSquared)
+sumofListXtimeY = addElementsInList(listXtimeY)
+n = len(list1)
 
 
 
