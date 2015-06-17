f8a11bd5bdb35e4b8f8494f1a34925c4009213aa
Nema Nemati <nenemati@gmail.com>
2015-06-17 01:31:46 -0400

    pandas

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pandas import DataFrame
 
 # df1 = pd.read_csv('Relocated_Vehicles.csv')
 # df1 = pd.read_csv('Towed_Vehicles.csv')
-#
+
 def square(list):
     newList = []
     for i in list:
@@ -59,9 +59,14 @@ def standardDeviationX(listX):
     n=len(listX)
     sigmaX = addElementsInList(listX)
     sigmaXThenSquared = math.pow(sigmaX,2)
-    numerator = (sigmaXSquared) - (1/n * sigmaXThenSquared)
+    numeratorOne = (sigmaXSquared)
+    numeratorTwo = (1/n * sigmaXThenSquared)
+    numerator = numeratorOne-numeratorTwo
+    print(numerator)
     final = numerator/(n-1)
+
     print(final)
+
 
 
 
@@ -102,3 +107,27 @@ standardDeviationX(list1)
 # f.close()
 #
 
+import pandas as pd
+import numpy as np
+
+relocated = pd.read_csv('Relocated_Vehicles.csv') #read in
+towed = pd.read_csv('Towed_Vehicles.csv') #read in
+TOTAL_l = len(relocated) #total
+TOTAL_t = len(towed)
+
+towed_makes = towed.groupby('Make')
+relocated_makes = relocated.groupby('Make')
+
+towed_perc = towed_makes.apply(lambda x: len(x) / float(TOTAL_t))
+relocated_perc = relocated_makes.apply(lambda x: len(x) / float(TOTAL_l))
+
+make_mapper = pd.Series(index=towed_perc.index)
+for name1 in make_mapper.index:
+    for name2 in relocated_perc.index:
+        if name1.lower() in name2.lower():
+            make_mapper[name1] = name2
+            #checks if shorter name is in longer name
+
+relocated_perc = make_mapper.map(relocated_perc)
+corr = relocated_perc.corr(towed_perc)
+print corr  # positively correlated, so cars that are likely to be towed are also likely to be relocated
